85fcc48d0e90ebc92de8f589420310f093144120
YunSeoHwan <48356954+YunSeoHwan@users.noreply.github.com>
2022-03-16 12:58:59 +0900

    Update bj5622.py

--- Algorithm/bj5622.py
+++ Algorithm/bj5622.py
@@ -1,10 +1,10 @@
 dial = input()
-change_list = []
-ascii_list = []
+change_list = []    # string을 list로 변환 리스트
+ascii_list = []     # list값 ascii로 변환 리스트
 sum = 0
 change_list = list(dial)
 
-for i in change_list:
+for i in change_list:       # ascii로 변환
   ascii_list.append(ord(i))
 
 for i in range(len(ascii_list)):
@@ -29,3 +29,16 @@ for i in range(len(ascii_list)):
   sum += ascii_list[i]
 
 print(sum)
+
+
+# 좋은 풀이법
+alpabet_list = ['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
+word = input()
+
+time = 0
+for unit in alpabet_list :  
+    for i in unit:  # alpabet 리스트에서 각 요소를 꺼내서 한글자씩 분리
+        for x in word :  # 입력받은 문자를 하나씩 분리
+            if i == x :  # 두 알파벳이 같으면
+                time += alpabet_list.index(unit) +3  # time = time + index + 3
+print(time)
