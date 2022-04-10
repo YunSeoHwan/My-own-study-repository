d4138a837678b5f25aa113db70c2707151ec8de1
YunSeoHwan <48356954+YunSeoHwan@users.noreply.github.com>
2022-04-10 21:33:58 +0900

    Add files via upload

---
+++ Basic/chapter02_01.py
@@ -0,0 +1,17 @@
+# chapter02_01
+# print 사용법
+
+# 기본 출력
+print('Python Start!')          # Python Start!
+print("Python Start!")          # Python Start!
+print('''Python Start!''')      # Python Start!
+print("""Python Start!""")      # Python Start!
+
+print()
+
+# separator 옵션
+print('P', 'Y', 'T', 'H', 'O', 'N', sep='')     # PYTHON
+print('010', '1234', '5678', sep='-')           # 010-1234-5678
+print('dbstjghks', 'naver.com', sep='@')        # dbstjghks@naver.com
+
+print()
