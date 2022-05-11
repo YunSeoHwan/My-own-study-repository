a7a0ddc44e9a4160b419fb00b088154ec0716a87
YunSeoHwan <48356954+YunSeoHwan@users.noreply.github.com>
2022-05-11 15:33:41 +0900

    Add files via upload

--- Basic/chapter03_03.py
+++ Basic/chapter03_03.py
@@ -29,3 +29,67 @@ print('d : ', e[-1][1:6])                   # ['Base', 'Captine']   -> 리스트
 print('c + d : ', c + d)                    # [70, 75, 80, 85, 1000, 10000, 'Ace', 'Base', 'Captine']
 print('c * 3 : ', c * 3)                    # [70, 75, 80, 85, 70, 75, 80, 85, 70, 75, 80, 85]
 print("'Test' + c[0]", 'Test' + str(c[0]))  # Test70
+
+# 값비교
+print(c == c[:3] + c[3:])                   # True
+print(c)                                    # [70, 75, 80, 85]
+print(c[:3] + c[3:])                        # [70, 75, 80, 85]
+
+# Identify(id)
+temp = c
+print(temp, c)                              # 같은 id값을 가짐
+print(id(temp))
+print(id(c))
+
+# 리스트 수정, 삭제
+c[0] = 4
+print('c : ', c)                            # [4, 75, 80, 85]
+
+c[1:2] = ['a', 'b', 'c']                    
+print('c : ', c)                            # [4, 'a', 'b', 'c', 80, 85]
+
+c[1:2] = [['a', 'b', 'c']]
+print('c : ', c)                            # [4, ['a', 'b', 'c'], 'b', 'c', 80, 85]
+
+c[1] = ['a', 'b', 'c']
+print('c : ', c)                            # [4, ['a', 'b', 'c'], 'b', 'c', 80, 85]
+
+c[1:3] = []                                 
+print('c : ', c)                            # [4, 'c', 80, 85]
+
+del c[2]
+print('c : ', c)                            # [4, 'c', 85]
+
+# 리스트 함수
+a = [5, 4 ,2 ,1, 3]
+
+a.append(10)
+print('a : ', a)                            # [5, 4, 2, 1, 3, 10]
+
+a.sort()
+print('a : ', a)                            # [1, 2, 3, 4, 5, 10]
+
+a.reverse()
+print('a : ', a)                            # [10, 5, 4, 3, 2, 1]
+print('a : ', a.index(3), a[3])             # 3 3  -> index값 호출하는 방법
+a.reverse()
+
+a.insert(2,7)   
+print('a : ', a)                            # [1, 2, 7, 3, 4, 5, 10]
+
+a.remove(10)
+print('a : ', a)                            # [1, 2, 7, 3, 4, 5]
+
+print('a : ', a.pop())                      # 5
+print('a : ', a)                            # [1, 2, 7, 3, 4]     -> last index값 불러오고 remove
+
+print('a : ', a.count(4))                   # 1  -> value 수 return
+
+ex = [8, 9]
+a.extend(ex)
+print('a : ', a)                            # [1, 2, 7, 3, 4, 8, 9]
+
+# 반복문 활용
+while a:
+    data = a.pop()
+    print(data)
