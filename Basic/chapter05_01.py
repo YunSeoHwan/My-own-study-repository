d7a1c8a4a6a496a913679ce598766f89972ff17b
YunSeoHwan <48356954+YunSeoHwan@users.noreply.github.com>
2022-05-28 23:55:46 +0900

    Add files via upload

--- Basic/chapter05_01.py
+++ Basic/chapter05_01.py
@@ -102,3 +102,19 @@ def nested_func(num):
 
 nested_func(100)                    # In func 200
 # func_in_func(1000)                # 호출 불가능
+
+# 람다식 예제
+# 메모리 절약, 가독성 향상, 코드 간결
+# 함수는 객체 생성 -> 리소스(메모리) 할당
+# 람다는 즉시 실행 함수(Heap 초기화) -> 메모리 초기화
+# 남발 시, 가독성 오히려 감소
+
+def mul_func(x, y):
+    return x * y
+
+print(mul_func(10, 50))             # 500
+
+mul_func_var = mul_func             # 객체 생성
+print(mul_func_var(20, 50))         # 1000
+
+lambda_mul_func = lambda x, y: x * y
