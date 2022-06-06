39f1406d9b2b3e83f7d5fac1db3c2e3372b42e5b
YunSeoHwan <48356954+YunSeoHwan@users.noreply.github.com>
2022-06-06 23:45:54 +0900

    Add files via upload

--- practice/class.py
+++ practice/class.py
@@ -1,4 +1,7 @@
 # 1
+import random
+
+
 class Human:
     pass
 
@@ -136,3 +139,47 @@ print(s.get_name())                         # 삼성전자
 print(s.get_code())                         # 005930
 
 # 11
+class Account:
+    def __init__(self, name, cash):
+        self.name = name
+        self.cash = cash
+        self.bank = 'sc은행'
+        num1 = random.randint(0, 999)
+        num2 = random.randint(0, 99)
+        num3 = random.randint(0, 999999)
+
+        num1 = str(num1).zfill(3)           # 1 -> '1' -> '001'
+        num2 = str(num2).zfill(2)
+        num3 = str(num3).zfill(6)
+
+        self.account_number = num1 + '-' + num2 + '-' + num3
+
+y = Account('윤서환', 1000)
+print(y.name, y.cash, y.bank, y.account_number)       # 윤서환 1000 sc은행 676-95-198616
+
+# 12
+class Account:
+
+    account_count = 0
+
+    def __init__(self, name, cash):
+        self.name = name
+        self.cash = cash
+        self.bank = 'sc은행'
+        num1 = random.randint(0, 999)
+        num2 = random.randint(0, 99)
+        num3 = random.randint(0, 999999)
+
+        num1 = str(num1).zfill(3)           # 1 -> '1' -> '001'
+        num2 = str(num2).zfill(2)
+        num3 = str(num3).zfill(6)
+
+        self.account_number = num1 + '-' + num2 + '-' + num3
+        Account.account_count += 1
+    
+    def get_account_num(self):
+        print(Account.account_count)
+
+y = Account('안녕', 100)
+t = Account('d', 100)
+t.get_account_num()
