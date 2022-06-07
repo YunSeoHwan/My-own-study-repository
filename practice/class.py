86220a37f009cb22b627ed20ad6bbc76b7e7bf77
YunSeoHwan <48356954+YunSeoHwan@users.noreply.github.com>
2022-06-07 21:08:11 +0900

    Add files via upload

--- practice/class.py
+++ practice/class.py
@@ -39,7 +39,7 @@ class Human:
         self.name = name
         self.age = age
         self.sex = sex
-
+    
     def who(self):
         print('이름 : {}, 나이 : {}, 성별 : {}'.format(self.name, self.age, self.sex))
     
@@ -77,8 +77,8 @@ y = Human('서환', 23, '남자')
 del y                                       # 나의 죽음을 알리지 마라
 
 # 7
-class OMG :
-    def print() :
+class OMG:
+    def print():
         print("Oh my god")
 
 
@@ -177,9 +177,103 @@ class Account:
         self.account_number = num1 + '-' + num2 + '-' + num3
         Account.account_count += 1
     
-    def get_account_num(self):
-        print(Account.account_count)
+    @classmethod
+    def get_account_num(cls):
+        print(cls.account_count)            # Account.account_count
 
-y = Account('안녕', 100)
-t = Account('d', 100)
-t.get_account_num()
+    def deposit(self, money):
+        if money >= 1:
+            self.cash += money
+    
+y = Account('윤', 100)
+y.deposit(100)
+print(y.cash)                               # 200
+
+# 13
+class Account:
+
+    account_count = 0
+
+    def __init__(self, name, cash):
+        self.name = name
+        self.cash = cash
+        self.bank = 'sc은행'
+        num1 = random.randint(0, 999)
+        num2 = random.randint(0, 99)
+        num3 = random.randint(0, 999999)
+
+        num1 = str(num1).zfill(3)           # 1 -> '1' -> '001'
+        num2 = str(num2).zfill(2)
+        num3 = str(num3).zfill(6)
+
+        self.account_number = num1 + '-' + num2 + '-' + num3
+        Account.account_count += 1
+    
+    @classmethod
+    def get_account_num(cls):
+        print(cls.account_count)            # Account.account_count
+
+    def deposit(self, money):
+        if money >= 1:
+            self.cash += money
+
+    def withdraw(self, money):
+        if self.cash - money >= 0:
+            self.cash -= money
+    
+y = Account('윤', 100)
+y.deposit(100)                              # cash : 200
+y.withdraw(200)                             # cash : 0
+print(y.cash)                               # 0
+
+# 14
+class Account:
+
+    account_count = 0
+    deposit_count = 0
+
+    def __init__(self, name, cash):
+        self.name = name
+        self.cash = cash
+        self.bank = 'sc은행'
+        num1 = random.randint(0, 999)
+        num2 = random.randint(0, 99)
+        num3 = random.randint(0, 999999)
+
+        num1 = str(num1).zfill(3)           # 1 -> '1' -> '001'
+        num2 = str(num2).zfill(2)
+        num3 = str(num3).zfill(6)
+
+        self.account_number = num1 + '-' + num2 + '-' + num3
+        Account.account_count += 1
+    
+    @classmethod
+    def get_account_num(cls):
+        print(cls.account_count)            # Account.account_count
+
+    def deposit(self, money):
+        if money >= 1:
+            self.cash += money
+            Account.deposit_count += 1
+
+            if Account.deposit_count % 5 == 0:      # 5회 입금마다 이자 1% 추가
+                self.cash *= 1.01
+
+
+    def withdraw(self, money):
+        if self.cash - money >= 0:
+            self.cash -= money
+    
+    def display_info(self):
+        print('은행이름 : ', self.bank)
+        print('예금주 : ', self.name)
+        print('계좌번호 : ', self.account_number)
+        print('잔고 : ', self.cash)
+    
+y = Account('윤', 500)
+y.deposit(100)
+y.deposit(100)
+y.deposit(100)
+y.deposit(100)
+y.deposit(100)
+print(y.cash)                                       # 1010.0
