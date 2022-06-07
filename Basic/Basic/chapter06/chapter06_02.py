c2ed524c2b87139976ccc1fe3b7bb08409428f20
YunSeoHwan <48356954+YunSeoHwan@users.noreply.github.com>
2022-06-07 21:51:15 +0900

    Add files via upload

---
+++ Basic/Basic/chapter06/chapter06_02.py
@@ -0,0 +1,27 @@
+# Chapter06_2
+# 파이썬 모듈
+# Module : 함수, 변수, 클래스 등 파이썬 구성 요소 등을 모아놓은 파일
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def power(x, y):
+    return x ** y
+
+print('-' * 15)
+print('called inner!!')
+print(add(5, 5))                        # 10
+print(subtract(15, 5))                  # 10
+print(divide(10, 2))                    # 5.0
+print(power(5, 3))                      # 125
+print('-' * 15)
+
