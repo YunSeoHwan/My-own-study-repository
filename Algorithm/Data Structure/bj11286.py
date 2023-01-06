5f1b7cbac0bf5ee04fd8e756920320c85904c118
YunSeoHwan <dbstjghks@naver.com>
2023-01-06 16:34:17 +0900

    docs : 주석달기

--- Algorithm/Data Structure/bj11286.py
+++ Algorithm/Data Structure/bj11286.py
@@ -3,15 +3,25 @@ import sys
 
 input = sys.stdin.readline
 n = int(input())
+
+# heap 저장 리스트
 heap = []
-abs_list = []
+
 for i in range(n):
     x = int(input())
 
+    # x가 0이 아니면, 절댓값과 입력값 동시 저장
+    # 절댓값을 우선적으로 정렬
     if x != 0:
-        heapq.heappush(heap, (abs(x), x))
+        heapq.heappush(heap, [abs(x), x])
+    
+    # 0 입력의 경우
     else:
+
+        # emtpy면 0 출력
         if len(heap) == 0:
             print(0)
+
+        # 원래 heap 원소 출력
         else:
             print(heapq.heappop(heap)[1])
