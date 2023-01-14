6b9e3b3a7d6fdecfeaf64aaedeabdde1af67db8c
YunSeoHwan <dbstjghks@naver.com>
2023-01-14 17:18:02 +0900

    add : 종이의 개수 분할정복 3번째 문제. 확실히 어려번 반복학습을 하니, 금방 구현했다. python이라 메모리나 시간을 많이 잡아먹는다.

---
+++ Algorithm/Data Structure/bj1780.py
@@ -0,0 +1,34 @@
+import sys
+
+input = sys.stdin.readline
+sys.setrecursionlimit(10000)
+n = int(input())
+
+matrix = [list(map(int, input().split())) for _ in range(n)]
+cnt = []
+def divide(x, y, n):
+    color = matrix[x][y]
+
+    # 색 같은지 판별
+    for i in range(x, x + n):
+        for j in range(y, y + n):
+
+            # 색깔이 다르면 분할정복
+            if color != matrix[i][j]:
+                for a in range(3):
+                    for b in range(3):
+                        divide(x + (n//3)*a, y + (n//3)*b, n//3)
+                return
+
+    # 색깔에 따라 개수 삽입
+    if color == 1:
+        cnt.append(1)
+
+    elif color == -1:
+        cnt.append(-1)
+
+    else:
+        cnt.append(0)
+
+divide(0, 0, n)
+print(cnt.count(-1), cnt.count(0), cnt.count(1), sep='\n')
